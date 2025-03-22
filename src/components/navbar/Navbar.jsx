d99b765dd0a562824dd49467ba8fca44b6ecff3f
Rush <itzashert@gmail.com>
2025-03-23 00:59:36 +0530

    1:22:17 left

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,26 @@
-import "./navbar.css"
-import {Link} from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="navContainer">
-      <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
-        <span className="logo">STAYVERSE</span></Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
-      </div>
-    </div>
-  )
-}
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">STAYVERSE</span>
+        </Link>
 
-export default Navbar
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
+      </div>
+    </div>   
+  );
+};
+
+export default Navbar;
---
+++ src/pages/login/Loginn.jsx
@@ -0,0 +1,87 @@
+import React, { useContext, useState } from "react";
+// import "./login.css";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "LOGIN_START" });
+
+    try {
+      const res = await axios.post(
+        "http://localhost:8800/api/auth/login",
+        credentials
+      );
+      // console.log("Login response data:", res.data); // Confirm response
+      // console.log("Storing in localStorage...");
+  
+      localStorage.setItem("user", JSON.stringify(res.data)); 
+  
+      // console.log("Stored in localStorage:", localStorage.getItem("user")); // Check if it's set
+  
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+
+      navigate("/");
+    } catch (err) {
+      dispatch({
+        type: "LOGIN_FAILURE",
+        payload: err.response?.data || "Something went wrong",
+      });
+      console.error("Login failed:", err);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          onChange={handleChange}
+          className="lInput"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+
+        <button
+          disabled={loading}
+          onClick={handleClick}
+          className="lButton"
+        >
+          {loading ? "Logging in..." : "Login"}
+        </button>
+
+        {error && <span className="errorMsg">{error.message || error}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
